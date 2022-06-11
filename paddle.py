a6029f4273787fc919fb81dedb520bb1a947485d
Jarred Taylor <jarrede20@gmail.com>
2022-06-11 10:33:18 -0400

    created the game borders, implemented a scoreboard, and tied it together

--- paddle.py
+++ paddle.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
+    
     def __init__(self, position):
         super().__init__()
         self.shape("square")
@@ -16,3 +18,4 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
+
